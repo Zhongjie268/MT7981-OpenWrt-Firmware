7dcb62358b915139e778c90d131fb287af06f37d
Zhongjie268 <441252827@qq.com>
2025-09-21 11:39:12 +0800

    Add files via upload

--- cmcc-rax3000m/hanwckf-immortalwrt/part2.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part2.sh
@@ -14,8 +14,6 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-
 echo '替换jerrykuku的luci argon主题'
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
@@ -25,26 +23,3 @@ echo '替换golang到1.22.x'
 rm -rf feeds/packages/lang/golang
 git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 echo '=========Replace golang OK!========='
-
-echo '替换Passwall软件'
-rm -rf feeds/luci/applications/luci-app-passwall/*
-git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
-mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
-rm -rf passwall
-echo '=========Replace passwall source OK!========='
-
-echo '添加Passwall2软件'
-# rm -rf feeds/luci/applications/luci-app-passwall2
-mkdir feeds/luci/applications/luci-app-passwall2
-git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 passwall2
-mv passwall2/luci-app-passwall2/* feeds/luci/applications/luci-app-passwall2/
-rm -rf passwall2
-echo '=========Replace passwall source OK!========='
-
-echo '修改Passwall检测规则'
-sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
-echo '=========ALTER passwall denpendcies check OK!========='
-
-echo '修改Passwall2检测规则'
-sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall2/root/usr/share/passwall2/app.sh
-echo '=========ALTER passwall denpendcies check OK!========='
